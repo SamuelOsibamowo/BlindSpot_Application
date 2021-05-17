5bd086e0257fded23b5230bc5f232381399fd3c8
Samuel Osibamowo <samuelosibamowo@gmail.com>
2021-05-16 22:43:20 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/blindspot_application/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.blindspot_application
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Vibrator
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        val learnButton = findViewById<Button>(R.id.learn_Button)
+        val practiceButton = findViewById<Button>(R.id.practice_Button)
+        val seeAllButton = findViewById<ImageButton>(R.id.seeAll_Button)
+
+        learnButton.setOnClickListener{
+            val intent = Intent(this,LearnPage::class.java)
+            startActivity(intent)
+        }
+
+        practiceButton.setOnClickListener{
+            val intent = Intent(this,PracticePage::class.java)
+            startActivity(intent)
+        }
+
+        seeAllButton.setOnClickListener{
+            val intent = Intent(this,SeeMorePage::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
+
+
+
